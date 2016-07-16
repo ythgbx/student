acbc36181a083c04b5913e44cb5c27095435142f
xiaoEight <ppalyq@126.com>
2016-07-16 14:36:38 +0800

    fix user collection line request

---
+++ src/main/java/net/bus/web/controller/dto/BaseRequest.java
@@ -0,0 +1,17 @@
+package net.bus.web.controller.dto;
+
+/**
+ * Created by sky on 16/7/16.
+ */
+public class BaseRequest {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
