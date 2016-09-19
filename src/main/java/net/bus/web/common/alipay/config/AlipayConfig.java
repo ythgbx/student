e948ca47dcc62c7b9d37f41400a919ddfac042cb
“吕焰文” <649924325@qq.com>
2016-09-19 18:41:25 +0800

    1、修改了卖家支付宝账号2、商户的私钥

--- src/main/java/net/bus/web/common/alipay/config/AlipayConfig.java
+++ src/main/java/net/bus/web/common/alipay/config/AlipayConfig.java
@@ -19,20 +19,20 @@ public class AlipayConfig {
 	public static String partner = "2088421421805384";
 
 	//商户的私钥,需要PKCS8格式，RSA公私钥生成：https://doc.open.alipay.com/doc2/detail.htm?spm=a219a.7629140.0.0.nBDxfy&treeId=58&articleId=103242&docType=1
-	public static String private_key ="MIICeQIBADANBgkqhkiG9w0BAQEFAASCAmMwggJfAgEAAoGBAMWimByhTal6fT13" +
-			"6BQvxJDH27tOukbGiW39OHwlb9mCYCTC6jo0HMCZXyewCAkhdfQ2RUU5q+yBF5st" +
-			"Lvw3L4jDjDWR+JItNvqYVI8Vqea5LTC0p3rLA/+d/VUXWVoG0pmb++xj21m0SiSo" +
-			"siH8eTcysj7SgW8Re7i1GvTA5nlTAgMBAAECgYEAvIG/sN8yIksRFgY+uLJvPOFE" +
-			"dr468rL/9ocmz1MuAjtW1PmNTALUwegIWOSQZZUi2tLA86x0jdr71ykbvbotfQdQ" +
-			"nMqMYakP5+Tlwk/ZkbtVO2pPDhCDEdbiATryyWMv1hOrJncEBBILcWgl1TqLrGpZ" +
-			"kqj5bVXHi6Qe/nIwumECQQD9chDiTGmM9Ml5/akyWcGRF/5a8erpUF/zKQxQck8g" +
-			"NPKwMKHvtzLRvSg0H2FHPw0oeqJnzx/a7YAwtamNl1u7AkEAx6CHCx/7fUk9eZfk" +
-			"9K+eFqH3jSUTpaSTcm3E5yhJygAwZUgVaPvIomtsRr3Dvr/Q4lhSCwdmtydp4kIl" +
-			"KoxjSQJBAKuJuZ3BBCuwPnF/2zXGBDo1GCXr2QgjK14D6V7Fj4KH876GnAQIZbAJ" +
-			"rB1jI886AKjmH+fNC4189CeDvQelayMCQQCMKaMH5aHPmuAGAudoAfKO05Qbzx+X" +
-			"WxGH85if8QK2SSGxy0X1G2wI1kIE/XJ1+PoyIm8gfUbabFsE2Qp9cGq5AkEAsSeB" +
-			"ArVUPzIfBcwU6XbXnnIrBEZcieYxgL681kKchxZ9W4VOY0SSAA9h9upRcoiYx1mh" +
-			"PdkrgRTXGc/zeRP8dw==";
+	public static String private_key ="MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAL9Ag08ls0ViI2QL" +
+			"fVwRvMaspfqiR8lfJUq/IVhaHaWbKxQgbNHfY1zCxdsjMJgKiaD5D9vEt3J8vfHS" +
+			"Y3QIo5l0CVSIALrJ6gw+4gZeVoNVQ/uuXdnDlEkkb/fPC+RGMvHzwUF7pkVX5k5Y" +
+			"NynqD39vN65hMoZyLB0gmtQ+BdgdAgMBAAECgYALJE0CDWY2QpNhPrIc7QOcZ257" +
+			"/XxFJP/dLOlaZlWX0kReNc1hJKx7OPWh4JjyIpsF/7yGxx0Q1GeUcPdOSTqOmjyK" +
+			"BVGv1fANSBru2rFP9Be8hYRnDM2S/GKni/jXQ0G+vreZpqbcoz4zqPuJxS9J3ZN3" +
+			"eAb+yeJd5aeU9jZRAQJBAOle3w7oZmwJtFhCSUlmrERKZ5kjd1HxOVHtnUvLsDyd" +
+			"BDQ5a0F36IcAgb0bkbRoOLhm6aDB+XMNkDjF+IPWYT0CQQDRzBnkILNV+M1dBXUY" +
+			"JOapeCES4umJmWuvQ7uPbZS32aZgBvDDMtXnGiwkFE13bSPKK8Dljf+YL5iSLOv3" +
+			"pwBhAkASIdDq85uhoU9l3CCdl2CjHbTW7S6EK13sw62JChZzXy/Ibv2qZIqokp2B" +
+			"9/ZmMgLWMQjN+MRM8CzYfGlGtQ1xAkBYlJut9ZyixQ04pHA/RH1buowEZGiEn5GX" +
+			"C7sqoPL0+WHFE8keMd2c2TEf/q6CqUM+PmEQy3bKs/XwaiUriIihAkBnwIiRAiyo" +
+			"XNBWgrQhDmmf7ya/wc5PeMAj4Oc7TvYTCC6dDz6NFd2yrcH9prl64393J8XTcIi3" +
+			"UE9uBWKmoHMq";
 	
 	//支付宝的公钥，查看地址：https://openhome.alipay.com/platform/keyManage.htm?keyType=partner
 	public static String alipay_public_key  = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCnxj/9qwVfgoUh/y2W89L6BkRAFljhNhgPdyPuBV64bfQNN1PjbCzkIM6qRdKBoLPXmKKMiFYnkd6rAoprih3/PrQEB/VsW8OoM8fxn67UDYuyBTqA23MML9q1+ilIZwBC2AQ2UBVOrFXfFl75p6/B5KsiNG9zpgmLCUYuLkxpLQIDAQAB";
@@ -53,7 +53,7 @@ public class AlipayConfig {
 	public static String notify_url = "http://service.zfbus.net/alipay/async";
 
 	//卖家支付宝账号
-	public static String seller_id ="2687137048@qq.com";
+	public static String seller_id ="whytxnkj@163.com";
 
 //↑↑↑↑↑↑↑↑↑↑请在这里配置您的基本信息↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
 }
