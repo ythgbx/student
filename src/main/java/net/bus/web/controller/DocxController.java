fd184ca967a80a2880dae918f4137a3a1ae59ab9
sakura <87719349@qq.com>
2016-12-20 13:26:41 +0800

    添加注释以及修改文件NIO为普通IO

--- src/main/java/net/bus/web/controller/DocxController.java
+++ src/main/java/net/bus/web/controller/DocxController.java
@@ -33,21 +33,28 @@ public class DocxController {
     @Autowired
     HttpServletResponse response;
 
+    /**
+     * XWPFParagraph：代表一个段落。
+     * XWPFRun：代表具有相同属性的一段文本。
+     * XWPFTable：代表一个表格。
+     * XWPFTableRow：表格的一行。
+     * XWPFTableCell：表格对应的一个单元格。
+     * XWPFDocument：整个文档 文档包含表格和段落 表格中又包含行 行中有单元格 单元格里有段落 段落里有文本段一层一层去取
+     * 拿到文本判断后进行替换就行了
+     * @return
+     */
     @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
     public Object DocxProcess(){
         String fileName = session.getServletContext().getRealPath("/resources/upload/学生个人建档申请表.docx");
-//        File file = new File(fileName);
-//        System.out.println(String.valueOf(file.exists()));
-//        System.out.println(request.getServletPath());
-        Path file = Paths.get(fileName);
-        if(Files.exists(file)){
+        File file = new File(fileName)
+        if(file.exists()){
             response.setContentType("application/octet-stream");
             response.addHeader("Content-Disposition",
                     "attachment; filename=" + "aaa.docx");
 
             try {
-                XWPFDocument document = new XWPFDocument(new FileInputStream(file.toFile()));
+                XWPFDocument document = new XWPFDocument(new FileInputStream(file));
                 for (XWPFParagraph p : document.getParagraphs()) {
                     List<XWPFRun> runs = p.getRuns();
                     if (runs != null) {
