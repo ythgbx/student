ae92a00eeb3c46796e781f6b27ae73af4b0f7684
sun_123 <731266793@qq.com>
2017-06-21 16:31:54 +0800

    上传遗漏类

---
+++ src/main/java/net/bus/web/mapper/MotivationalMapper.java
@@ -0,0 +1,39 @@
+package net.bus.web.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import net.bus.web.model.MotivationPovo;
+import net.bus.web.model.Motivational;
+import net.bus.web.model.MotivationalExample;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.session.RowBounds;
+
+public interface MotivationalMapper {
+    int countByExample(MotivationalExample example);
+
+    int deleteByExample(MotivationalExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Motivational record);
+
+    int insertSelective(Motivational record);
+
+    List<Motivational> selectByExampleWithRowbounds(MotivationalExample example, RowBounds rowBounds);
+
+    List<Motivational> selectByExample(MotivationalExample example);
+
+    Motivational selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Motivational record, @Param("example") MotivationalExample example);
+
+    int updateByExample(@Param("record") Motivational record, @Param("example") MotivationalExample example);
+
+    int updateByPrimaryKeySelective(Motivational record);
+
+    int updateByPrimaryKey(Motivational record);
+
+    //    查询励志奖学金
+    List<MotivationPovo> selectMotivation(Map map);
+}
---
+++ src/main/java/net/bus/web/model/Motivational.java
@@ -0,0 +1,205 @@
+package net.bus.web.model;
+
+import java.util.Date;
+
+public class Motivational {
+    private Long id;
+
+    private String idcard;
+
+    private Integer ranking;
+
+    private Integer tnumber;
+
+    private String populationcount;
+
+    private String familyincome;
+
+    private String monthlyincome;
+
+    private String revenuesource;
+
+    private String poorgrades;
+
+    private String amounted;
+
+    private String fundedpurpose;
+
+    private String fundpurpose;
+
+    private String bankaccount;
+
+    private String applicationreasons;
+
+    private Date remarks;
+
+    private Integer counselorreview;
+
+    private String counselorreviewremarks;
+
+    private Integer admin;
+
+    private String adminremarks;
+
+    private Date applicationtime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIdcard() {
+        return idcard;
+    }
+
+    public void setIdcard(String idcard) {
+        this.idcard = idcard;
+    }
+
+    public Integer getRanking() {
+        return ranking;
+    }
+
+    public void setRanking(Integer ranking) {
+        this.ranking = ranking;
+    }
+
+    public Integer getTnumber() {
+        return tnumber;
+    }
+
+    public void setTnumber(Integer tnumber) {
+        this.tnumber = tnumber;
+    }
+
+    public String getPopulationcount() {
+        return populationcount;
+    }
+
+    public void setPopulationcount(String populationcount) {
+        this.populationcount = populationcount;
+    }
+
+    public String getFamilyincome() {
+        return familyincome;
+    }
+
+    public void setFamilyincome(String familyincome) {
+        this.familyincome = familyincome;
+    }
+
+    public String getMonthlyincome() {
+        return monthlyincome;
+    }
+
+    public void setMonthlyincome(String monthlyincome) {
+        this.monthlyincome = monthlyincome;
+    }
+
+    public String getRevenuesource() {
+        return revenuesource;
+    }
+
+    public void setRevenuesource(String revenuesource) {
+        this.revenuesource = revenuesource;
+    }
+
+    public String getPoorgrades() {
+        return poorgrades;
+    }
+
+    public void setPoorgrades(String poorgrades) {
+        this.poorgrades = poorgrades;
+    }
+
+    public String getAmounted() {
+        return amounted;
+    }
+
+    public void setAmounted(String amounted) {
+        this.amounted = amounted;
+    }
+
+    public String getFundedpurpose() {
+        return fundedpurpose;
+    }
+
+    public void setFundedpurpose(String fundedpurpose) {
+        this.fundedpurpose = fundedpurpose;
+    }
+
+    public String getFundpurpose() {
+        return fundpurpose;
+    }
+
+    public void setFundpurpose(String fundpurpose) {
+        this.fundpurpose = fundpurpose;
+    }
+
+    public String getBankaccount() {
+        return bankaccount;
+    }
+
+    public void setBankaccount(String bankaccount) {
+        this.bankaccount = bankaccount;
+    }
+
+    public String getApplicationreasons() {
+        return applicationreasons;
+    }
+
+    public void setApplicationreasons(String applicationreasons) {
+        this.applicationreasons = applicationreasons;
+    }
+
+    public Date getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(Date remarks) {
+        this.remarks = remarks;
+    }
+
+    public Integer getCounselorreview() {
+        return counselorreview;
+    }
+
+    public void setCounselorreview(Integer counselorreview) {
+        this.counselorreview = counselorreview;
+    }
+
+    public String getCounselorreviewremarks() {
+        return counselorreviewremarks;
+    }
+
+    public void setCounselorreviewremarks(String counselorreviewremarks) {
+        this.counselorreviewremarks = counselorreviewremarks;
+    }
+
+    public Integer getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(Integer admin) {
+        this.admin = admin;
+    }
+
+    public String getAdminremarks() {
+        return adminremarks;
+    }
+
+    public void setAdminremarks(String adminremarks) {
+        this.adminremarks = adminremarks;
+    }
+
+    public Date getApplicationtime() {
+        return applicationtime;
+    }
+
+    public void setApplicationtime(Date applicationtime) {
+        this.applicationtime = applicationtime;
+    }
+}
